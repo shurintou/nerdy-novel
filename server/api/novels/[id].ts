be3f0158ecf9d422706599d9c990a8820513a79b
77Vincent <wentianqi77@outlook.com>
2024-09-15 23:20:47 +0900

    display real content

--- server/api/novels/[id].ts
+++ server/api/novels/[id].ts
@@ -1,31 +1,105 @@
 import { H3EventContext } from 'h3'
 import { NovelWithCurrentChapterContent, Chapter } from '@/types/apis/novels/'
+import getDBClient from "~/server/utils/db";
+import {QueryCommand} from "@aws-sdk/client-dynamodb";
 
-export default defineEventHandler((event: H3EventContext): NovelWithCurrentChapterContent => {
-  const { id } = event.context.params as { id: string }
-  const fooFlag = Math.random() < 0.5
-  const chapters: Array<Chapter> = []
-  for (let i = 1; i <= 10; i++) {
-    chapters.push({
-      id: `${i}`,
-      title: '第${i}章',
-      content: `<p>小说${id}的第${i}章内容</p>`,
-      order: i,
-      createdAt: '2024/01/01',
-      updatedAt: '2024/09/12',
-    })
-  }
-  return {
-    id: `${id}`,
-    author: fooFlag ? '王二狗' : '张狗蛋',
-    category: fooFlag ? '都市' : '言情',
-    description: fooFlag ? '有人选择在猩红的夜晚，饮上一杯烈酒，举起了手里的猎枪。' : '身上留下了无数伤病的夏临终于混到一条WBA普通拳王金腰带,身上留下了无数伤病的夏临终于混到一条WBA普通拳王金腰带,身上留下了无数伤病的夏临终于混到一条WBA普通拳王金腰带',
-    title: fooFlag ? '重生神拳' : '网络神豪',
-    chapters: chapters,
-    currentChapterContent: `<p>你点选章节的小说${id}内容</p>`,
-    imageUrl: fooFlag ? 'https://bookcover.yuewen.com/qdbimg/349573/1035114945/90.webp' : 'https://bookcover.yuewen.com/qdbimg/349573/1028463573/90.webp',
-    imageAlt: `Novel Thumbnail ${id}`,
-    createdAt: '2024/01/01',
-    updatedAt: '2024/09/12'
-  }
+export default defineEventHandler(async (event: H3EventContext): NovelWithCurrentChapterContent => {
+    // fetch novel meta
+    const { id } = event.context.params as { id: string }
+    let novel
+    let chapters: Array<Chapter> = []
+
+    try {
+        const client = await getDBClient();
+        const data = await client.send(new QueryCommand({
+            TableName: "nerdy-novel",
+            KeyConditionExpression: "id = :id",
+            ExpressionAttributeValues: {
+                ":id": {
+                    S: id,
+                }
+            }
+        }))
+        const {$metadata: {httpStatusCode}, Count, Items} = data
+
+        // TODO: handle the 4XX/5XX error case
+        if (httpStatusCode !== 200) {
+            return httpStatusCode
+        }
+
+        if (!Count) {
+            return 404
+        }
+
+        novel = Items[0]
+    } catch (e) {
+        console.log(e)
+        return
+    }
+
+    // fetch novel chapters
+    try {
+        const client = await getDBClient();
+        const data = await client.send(new QueryCommand({
+            TableName: "nerdy-novel-chapters",
+            KeyConditionExpression: "novel_id = :novel_id",
+            ExpressionAttributeValues: {
+                ":novel_id": {
+                    S: id,
+                }
+            }
+        }))
+        const {$metadata: {httpStatusCode}, Count, Items} = data
+
+        // TODO: handle the 4XX/5XX error case
+        if (httpStatusCode !== 200) {
+            return httpStatusCode
+        }
+        if (!Count) {
+            return 404
+        }
+
+        for (let chapter of Items) {
+            const {
+                id: {N: id},
+                title: {S: title},
+                content: {S: content},
+            } = chapter
+
+            chapters.push({
+                id: `${id}`,
+                title,
+                content: `<pre>${content}</pre>`, // TODO: styling
+                order: id,
+                createdAt: '2024/01/01', // TODO: what is the timestamp of chapter
+                updatedAt: '2024/09/12', // TODO: same comment as above
+            })
+        }
+    } catch (e) {
+        console.log(e)
+        return
+    }
+
+    const {
+        author_id: {N: author_id},
+        genre: {SS: genre},
+        synopsis: {S: synopsis},
+        title: {S: title},
+        updated_at: {N: updatedAt},
+        created_at: {N: createdAt},
+    } = novel
+
+    return {
+        id: `${id}`,
+        author: author_id,
+        category: genre,
+        description: synopsis,
+        title,
+        chapters,
+        currentChapterContent: chapters[0].content, // TODO: redundant
+        imageUrl: 'https://bookcover.yuewen.com/qdbimg/349573/1028463573/90.webp',
+        imageAlt: `Novel Thumbnail ${id}`,
+        createdAt,
+        updatedAt,
+    }
 })
