1bed74f94642574811b30fb4144d2ed12b25c291
shurintou <shurintou@gmail.com>
2024-09-19 16:24:40 +0900

    refactor: - removed NovelWithCurrentChapterContent interface and updated relative codes. - removed comment logic from novels/[id].vue. - updated useHead's parameter.

--- types/apis/novels/index.d.ts
+++ types/apis/novels/index.d.ts
@@ -4,10 +4,6 @@ import { Chapter } from '@/types/models/chapter.d.ts'
 
 export type { Novel, Comment, Chapter }
 
-export interface NovelWithCurrentChapterContent extends Novel {
-  currentChapterContent: string
-}
-
 export interface NovelMetaData extends Omit<Novel, 'chapters' | 'createdAt'> { }
 
 export interface NovelBasicMetaData extends Omit<Novel, 'chapters' | 'createdAt' | 'imageUrl' | 'imageAlt' | 'description'> { }
