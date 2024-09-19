04bf450b95dadb9cc862370a05e1e89cd6e9d83b
shurintou <shurintou@gmail.com>
2024-09-19 16:03:38 +0900

    feat: update homepage and modify header navigation. - removed the existing homepage's index.vue. - changed novels/index.vue as the new homepage's page component. - removed navigation links from Header.vue and added click-to-home navigation on the title.

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,19 +1,21 @@
 <template>
   <header>
     <div class="main-header header-title">
-      <h1>呆书网</h1>
+      <h1 class="home-page-navigation" @click="homePageNavigation">呆书网</h1>
     </div>
     <div class="header-content">
       <nav>
-        <ul>
-          <li><a :href="`/`">主页</a></li>
-          <li><a :href="`/novels`">全部作品</a></li>
-        </ul>
       </nav>
       <ToggleDarkModeButton />
     </div>
   </header>
 </template>
+
+<script lang="ts" setup>
+const homePageNavigation = () => {
+  window.location.href = '/'
+}
+</script>
 
 
 <style lang="css" scoped>
@@ -49,35 +51,15 @@ nav {
   flex-grow: 1;
 }
 
-nav ul {
-  list-style: none;
-  display: flex;
-  justify-content: center;
-  gap: 20px;
-  padding: 0;
-  margin: 0;
-}
-
-nav ul li {
-  display: inline;
-}
-
-nav ul li a {
-  text-decoration: none;
-  color: #f8f8f8;
-  font-weight: 500;
-  padding: 8px 16px;
-}
-
-nav ul li a:hover {
-  color: var(--primary-button-color);
-}
-
 #app-container.dark-mode header {
   background: var(--header-bg-dark);
 }
 
 #app-container.dark-mode header {
   background: var(--header-bg-dark);
+}
+
+.home-page-navigation {
+  cursor: pointer;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,43 @@
 <template>
-  <section class="hero">
-    <SlideComponent :apiPath="slideAPI" />
+  <section class="novel-list-page">
+    <NovelFilter />
+
+    <div v-if="selectedCategory" class="novel-list-wrapper">
+      <BaseList class="novel-list">
+        <BaseUnderline :level="3">小说列表</BaseUnderline>
+        <BasicNovelItem v-for="novel in novels" :key="novel.id" :novel-data="novel" />
+      </BaseList>
+      <pagination :total="totalPages" :current="page" @page-changed="fetchNovels" />
+    </div>
   </section>
-
-  <div class="main-content">
-    <section class="latest-novels">
-      <BaseUnderline :level="2">最新小说</BaseUnderline>
-      <BaseGrid>
-        <NovelItem v-for="item in novels" :key="item.id" :novelData="item" />
-      </BaseGrid>
-    </section>
-
-    <section class="popular-categories">
-      <h2>热门分类</h2>
-      <BaseGrid>
-        <CategoryItem v-for="item in categoryNovels" :key="item.id" :data="item" />
-      </BaseGrid>
-    </section>
-  </div>
 </template>
 
 <script lang="ts" setup>
-import type { NovelMetaData } from '@/types/apis/novels/'
-import type { CategoryNovels } from '@/types/apis/home/'
+import type { NovelBasicMetaData, CategoryNovelsData } from '@/types/apis/novels/'
+import Pagination from '@/components/base/Pagination.vue'
+import BasicNovelItem from '~/components/novel/BasicNovelItem.vue';
 
-const slideAPI = '/api/home/slides'
-const novelsAPI = '/api/home/novels'
-const categoriesAPI = '/api/home/categories'
+const totalPages = ref(0)
+const { page, selectedCategory, getNovelFilterSearchQuery, fetchNovels } = useNovelFilter()
+const novels = ref<Array<NovelBasicMetaData>>([])
 
-const { data: novels } = await useCacheFetch<Array<NovelMetaData>>(novelsAPI)
-const { data: categoryNovels } = await useCacheFetch<CategoryNovels[]>(categoriesAPI)
+const { data } = await useCacheFetch<CategoryNovelsData>(`/api/novels?${getNovelFilterSearchQuery()}`)
+novels.value = data.value?.novels || []
+totalPages.value = data.value?.totalPages || 0
 
 useHead({
   title: '主页 - 呆书网'
 })
+
 </script>
 
 <style lang="css" scoped>
-.hero {
+.novel-list-wrapper {
   max-width: 1200px;
   margin: 0 auto;
-  padding: 10px 0px 0px 0px;
 }
 
-.main-content {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 10px 20px;
+.novel-list {
+  padding: 0px 15px
 }
 </style>
