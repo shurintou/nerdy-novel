2624ce45a7afba433d865fa0e4a0fc183ece503f
77Vincent <wentianqi77@outlook.com>
2024-09-21 18:29:01 +0900

    category(genre) filter works

--- server/api/novels/index.ts
+++ server/api/novels/index.ts
@@ -5,11 +5,12 @@ import { getNovelList } from "~/server/utils/novel_list"
 const sizePerPage = 30
 
 export default defineEventHandler(async (event: H3Event<EventHandlerRequest>): Promise<CategoryNovelsData> => {
-    const {novels, total} = await getNovelList()
     const {category, page} = getQuery(event) as CategoryNovelsParams
+    const genre = category?.split(',') || [] // genre is the backend model name
+    const {novels, total} = await getNovelList(genre)
 
     return {
-        category: category?.split(',') || "none",
+        category: genre,
         page: page || 1,
         totalPages: Math.ceil(total / sizePerPage),
         total,
--- server/utils/novel_list.ts
+++ server/utils/novel_list.ts
@@ -1,21 +1,63 @@
-import { ScanCommand } from "@aws-sdk/client-dynamodb";
-import { NovelList, NovelMetaData } from "~/types/apis/home";
+import {ScanCommand} from "@aws-sdk/client-dynamodb";
+import {NovelList, NovelMetaData} from "~/types/apis/home";
 import getDBClient from "~/server/utils/db";
 
+const genreKey = "#genre"
+const genre1 = ":genre1"
+const genre2 = ":genre2"
+const genre3 = ":genre3"
 
-export async function getNovelList(): Promise<NovelList> {
+const filterExpression1 = `contains(${genreKey}, ${genre1})`
+const filterExpression2 = `contains(${genreKey}, ${genre1}) AND contains(${genreKey}, ${genre2})`
+const filterExpression3 = `contains(${genreKey}, ${genre1}) AND contains(${genreKey}, ${genre2}) AND contains(${genreKey}, ${genre3})`
+
+export async function getNovelList(genre: string[]): Promise<NovelList> {
     // var definition
     let data = []
     let total = 0
+    let genreValues = {}
+    let filter = {
+        FilterExpression: filterExpression1, // by default only one genre to filter with
+        ExpressionAttributeNames: {
+            [genreKey]: "genre",
+        },
+        ExpressionAttributeValues: genreValues,
+    }
+
+    switch (genre.length) {
+        case 1:
+            filter.FilterExpression = filterExpression1;
+            break;
+        case 2:
+            filter.FilterExpression = filterExpression2;
+            break;
+        case 3:
+            filter.FilterExpression = filterExpression3;
+            break;
+    }
+
+    if (genre.length > 0) {
+        genre.forEach((v, i) => {
+            genreValues[`:genre${i+1}`] = { S: v }
+        })
+    }
+
+    let params = {
+        TableName: "nerdy-novel",
+        Limit: 16, // do not show all
+    }
+    if (genre.length > 0) {
+        params = {
+            ...params,
+            ...filter,
+        }
+    }
 
     // data fetching
     try {
         const client = await getDBClient()
         // TODO: Count can be used for pagination
-        const { $metadata: { httpStatusCode }, Count, Items } = await client.send(new ScanCommand({
-            TableName: "nerdy-novel",
-            Limit: 16, // do not show all
-        }))
+        const {$metadata: {httpStatusCode}, Count, Items} = await client.send(new ScanCommand(params))
 
         // TODO: handle the 4XX/5XX error case
         if (httpStatusCode !== 200) {
@@ -32,13 +74,13 @@ export async function getNovelList(): Promise<NovelList> {
     const novels: Array<NovelMetaData> = []
     for (let v of data) {
         const {
-            id: { S: id },
-            author_id: { N: author_id },
-            genre: { SS: genre },
-            synopsis: { S: synopsis },
-            img_url: { S: imageUrl },
-            title: { S: title },
-            updated_at: { N: updatedAt },
+            id: {S: id},
+            author_id: {N: author_id},
+            genre: {SS: genre},
+            synopsis: {S: synopsis},
+            img_url: {S: imageUrl},
+            title: {S: title},
+            updated_at: {N: updatedAt},
         } = v
 
         novels.push({
