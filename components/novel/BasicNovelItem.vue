28bf6fa5f10e5e0f3e90cee91673118566455587
shurintou <shurintou@gmail.com>
2024-09-23 03:42:10 +0900

    Revert "tailwind" introduction. This reverts commit d82ae80c76d37e557015754992f07f92a4b5624f and d5888451e25a6df66577a362ce5c57a579eddd57.

--- components/novel/BasicNovelItem.vue
+++ components/novel/BasicNovelItem.vue
@@ -1,32 +1,11 @@
 <template>
-  <div class="basic-novel-list-item drop-shadow-md rounded flex mb-2 p-3" @click="clickHandler(novelData.id)">
-    <img class="novel-thumbnail mr-3 rounded" :src="novelData.imageUrl" :alt="novelData.imageAlt">
-
-    <div class="flex-auto flex flex-col justify-between">
-      <div class="flex">
-        <div class="text-xl font-bold cursor-pointer underline">
-          {{ novelData.title }}
-        </div>
-
-        <div class="flex-auto">
-          <div class="float-end">
-            <BaseTag v-for="category in novelData.categories" :text="category" />
-          </div>
-        </div>
-      </div>
-
-      <div class="text-sm text-gray-600">
-        {{ novelData.description }}
-      </div>
-
-      <div class="flex">
-        <div class="text-xs text-cyan-800 italic">{{ novelData.author }}</div>
-
-        <div class="flex-auto">
-          <div class="text-xs text-gray-400 italic float-end">更新于：{{ formattedDate }}</div>
-        </div>
-      </div>
+  <div class="basic-novel-list-item" @click="clickHandler(novelData.id)">
+    <div class="novel-title">
+      <BaseTag v-for="category in novelData.categories" :text="category" />
+      {{ novelData.title }}
     </div>
+    <div class="novel-author">{{ novelData.author }}</div>
+    <div class="novel-updated">{{ formattedDate }}</div>
   </div>
 </template>
 
@@ -51,12 +30,17 @@ const formattedDate = computed(() => {
 </script>
 
 <style lang="css" scoped>
-.novel-thumbnail {
-  width: 100px;
-}
-
 .basic-novel-list-item {
   background-color: var(--novel-background-color-light);
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 8px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  height: 60px;
+  margin: 4px;
+  cursor: pointer;
 }
 
 .basic-novel-list-item:hover {
@@ -68,11 +52,40 @@ const formattedDate = computed(() => {
   margin-bottom: 16px;
 }
 
+.novel-title {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  font-size: 0.975rem;
+  font-weight: bold;
+  color: var(--text-color-light);
+}
+
+.novel-author {
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+  font-size: 0.775rem;
+  color: #666;
+}
+
+.novel-updated {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  font-size: 0.775rem;
+  color: #999;
+}
+
 #app-container.dark-mode .basic-novel-list-item {
   background-color: var(--novel-background-color-dark);
 }
 
 #app-container.dark-mode .basic-novel-list-item:hover {
   background-color: #3a3a3a;
+}
+
+#app-container.dark-mode .novel-title {
+  color: var(--text-color-dark);
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,12 +17,5 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css' },
       ],
     }
-  },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+  }
 })
